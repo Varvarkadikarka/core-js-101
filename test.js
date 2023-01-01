b9033c1f51353561f5744dc7e8cde8b65f148ee1
Varvara <varyagagarina@gmail.com>
2023-01-02 02:44:20 +0300

    feat: 04-05-06-07 tasks

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
-function swapHeadAndTail(arr) {
-  const center = Math.floor(arr.length / 2);
-  const left = arr.slice(0, center);
-  const middle = arr.slice(center, -center);
-  const right = arr.slice(-center);
-  console.log([...right, ...middle, ...left]);
+function getDigitalRoot(num) {
+  return num
+    .toString()
+    .split('')
+    .map(Number)
+    .reduce((a, b) => a + b)
+    
 }
 
-swapHeadAndTail([ 1, 2, 3]);
+console.log(getDigitalRoot(45121232));
